2410f03f13331cd0f9a52d17552c72cc9b996146
Pallab Panda <pallab.panda@gmail.com>
2023-12-08 19:07:49 +0530

    Till List and Prompt

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ prompt = "Enter a ToDo -:"
 user_prompt1 = input(prompt)
 user_prompt2 = input(prompt)
 user_prompt3 = input(prompt)
+
+#list
 todos = [user_prompt1, user_prompt2, user_prompt3]
 print(todos)
+print(type(todos))
+
+
