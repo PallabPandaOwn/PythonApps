fbab3aa195f12052d6662fcaa46e4978c3425a48
Pallab Panda <pallab.panda@gmail.com>
2023-12-08 22:39:48 +0530

    end of day1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#user prompt 
 prompt = "Enter a ToDo -:"
 user_prompt1 = input(prompt)
 user_prompt2 = input(prompt)
@@ -7,5 +8,3 @@ user_prompt3 = input(prompt)
 todos = [user_prompt1, user_prompt2, user_prompt3]
 print(todos)
 print(type(todos))
-
-
