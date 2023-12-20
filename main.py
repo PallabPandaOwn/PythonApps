1e3d3d83c98d0d9565f676fcd9d9e91f3a92cd83
Pallab Panda <pallab.panda@gmail.com>
2023-12-20 19:24:58 +0530

    Todo progress

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-#user prompt 
-prompt = "Enter a ToDo -:"
-user_prompt1 = input(prompt)
-user_prompt2 = input(prompt)
-user_prompt3 = input(prompt)
+# #user prompt
+todos = []
+while True:
+    user_prompt = input("Add or Show or Exit: ")
+    user_prompt=user_prompt.strip()
+    match user_prompt:
+        case    "add":
+            item = input("Enter item to add in todo list:")
+            item= item.strip()
+            todos.append(item)
+        case    "show":
+            print("Todo list contains:")
+            for item in todos:
+                print(item)
+        case    "exit":
+            print("Bye!")
+            break
 
-#list
-todos = [user_prompt1, user_prompt2, user_prompt3]
-print(todos)
-print(type(todos))
+
+
+
+# prompt = "Enter a ToDo -:"
+#
+# todos = []
+#
+# while True:
+#     user_prompt1 = input(prompt)
+#     todos.append(user_prompt1)
+#     print(todos)
+# name = "pallab"
+# while True:
+#
+#     print(name.capitalize())
+import upper
+
+# while True:
+#     name = input("Enter name -")
+#     up =name.upper()
+#     print(up)
+
